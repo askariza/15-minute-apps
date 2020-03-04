50f35003445c6bb3ec12930aae46461946a0af55
askariza <49247375+askariza@users.noreply.github.com>
2020-03-03 21:20:57 -0800

    Update test.py

--- calculator/test.py
+++ calculator/test.py
@@ -4,16 +4,35 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtTest import QTest
 from PyQt5.QtCore import *
-import minesweeper
+import calculator
 
 app = QApplication(sys.argv)
 
-class PosTest(unittest.TestCase):
+class TestCalc(unittest.TestCase):
 	
+	self.pushButton_ac.pressed.connect(self.reset)
 	def setUp(self):
-		self.form = minesweeper.MainWindow()
+        	self.form = calculator.MainWindow()
+	def test_ac(self):
+		# store the state of the ac button
+		ac_state_before = self.form.pushButton_ac.isChecked()
+		# geting the AC botton 
+		ac_button = self.form.layout().itemAt(2).widget().widgetForAction(self.form.pushButton_ac)
+		# left click the button
+		QTest.mouseClick(ac_button, Qt.LeftButton)
+		# store the state of the ac button
+		button_state_after_left = self.form.pushButton_ac.isChecked()
+		# assert that the button state is not the same before and after the click
+		self.assertNotEqual(button_state_before, button_state_after_left)
+		# right click the button
+		QTest.mouseClick(ac_button, Qt.RightButton)
+		# store the state of the ac button
+		button_state_after_right = self.form.pushButton_ac.isChecked()
+		# assert that the button state is not the same before and after the click
+		self.assertEqual(button_state_after_left, button_state_after_right)
+		
 
-	def test_reset_function(self):
+	
 
 
 
